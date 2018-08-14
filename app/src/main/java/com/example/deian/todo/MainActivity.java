a577817533096cc6e500160df5e292c74ff7205b
unknown <deianp17@gmail.com>
2018-08-14 23:00:18 +0300

    Added action bar with appropriate icons

--- app/src/main/java/com/example/deian/todo/MainActivity.java
+++ app/src/main/java/com/example/deian/todo/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.deian.todo;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.method.KeyListener;
 import android.view.KeyEvent;
+import android.view.Menu;
 import android.view.View;
 import android.view.inputmethod.InputMethod;
 import android.view.inputmethod.InputMethodManager;
@@ -30,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         mEditText = findViewById(R.id.plain_text_input);
         mEditText.setVisibility(View.GONE);
@@ -37,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
         mEditText.setKeyListener(null);
 
         mFloatingActionButton = findViewById(R.id.fab);
+
+        mStringArray = FileHelper.readData(this);
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mStringArray);
         mListView = findViewById(R.id.listView);
         mListView.setAdapter(adapter);
@@ -48,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
                 openKeyboard();
             }
         });
+
 
         mEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -63,7 +71,8 @@ public class MainActivity extends AppCompatActivity {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
                         (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                    mStringArray.add(mEditText.getText().toString());
+                    adapter.add(mEditText.getText().toString());
+                    FileHelper.writeData(mStringArray, getApplicationContext());
                     closeKeyboard();
 
                     return true;
@@ -74,9 +83,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return true;
+    }
+
     private void openKeyboard(){
         mEditText.setKeyListener(mKeyListener);
         mEditText.requestFocus();
+        mEditText.setBackgroundColor(getResources().getColor(R.color.PrimaryGrey));
+        mEditText.setTextColor(Color.WHITE);
 
         InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.showSoftInput(mEditText, InputMethodManager.SHOW_IMPLICIT);
